8efc779ad3e6389d4b821d7c63f3faa4f4013d43
0GiS0 <giselatb@outlook.com>
2024-02-10 15:07:24 +0000

    Add CI workflow and update image conversion logic

--- ConvertImageToPng.cs
+++ ConvertImageToPng.cs
@@ -2,6 +2,7 @@ using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
 
 namespace tour_of_heroes_background
 {
@@ -10,18 +11,13 @@ namespace tour_of_heroes_background
         [FunctionName("ConvertImageToPng")]
         public void Run([BlobTrigger("alteregos/{name}.jpeg", Connection = "AzureStorageConnection")] Stream myBlob, string name, ILogger log,
             [Blob("alteregos/{name}.png", FileAccess.Write, Connection = "AzureStorageConnection")] Stream outputBlob)
-        {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
-
+        {            
             log.LogInformation($"Converting {name}.jpeg to {name}.png");
-
-            using var image = Image.Load(myBlob);
-            using var outputStream = new MemoryStream();
-
-            image.SaveAsPng(outputStream);
-            outputStream.Position = 0;
-
-            outputStream.CopyTo(outputBlob);
+            
+            using (var image = Image.Load(myBlob))
+            {
+                image.SaveAsPng(outputBlob);
+            }          
         }
     }
 }
